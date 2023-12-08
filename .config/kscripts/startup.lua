e1776320c6c7eb5a8f39ff382a2d197acfe545b3
khamui <khamui@me.com>
2023-12-08 21:29:43 +0100

    quick commit dotfiles.

---
+++ .config/kscripts/startup.lua
@@ -0,0 +1,5 @@
+print('startup loaded..')
+os.execute('tmux send-keys "cd ~/Repos/amapet/frontend && npm start" C-m')
+os.execute('tmux split-window -h;')
+os.execute('tmux send-keys "cd ~/Repos/amapet/backend && npm start" C-m')
+print('startup loaded..')
